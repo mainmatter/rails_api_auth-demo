389e3c622905653fdb9d0c899d779af70c2af8fa
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-07-15 10:40:09 +0200

    cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,5 @@ class User < ActiveRecord::Base
   attr_accessor :password
 
   has_one :login
-  has_one :private_address, class_name: 'Address'
-  has_one :work_address,    class_name: 'Address'
 
 end
