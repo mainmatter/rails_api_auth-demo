fe18b3d9cf42edb6c6152b20ef33d62ad054ae06
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-08-03 21:41:23 +0200

    fixed configuration

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -1,6 +1,8 @@
-Rails.application.config.x.facebook.tap do |facebook|
-  facebook.app_id       = ENV['FACEBOOK_APP_ID']
-  facebook.app_secret   = ENV['FACEBOOK_APP_SECRET']
-  facebook.graph_url    = 'https://graph.facebook.com/v2.3'
-  facebook.redirect_uri = 'http://localhost:3000/'
+RailsApiAuth.tap do |raa|
+  raa.user_model_relation = :user
+
+  raa.facebook_app_id       = ENV['FACEBOOK_APP_ID']
+  raa.facebook_app_secret   = ENV['FACEBOOK_APP_SECRET']
+  raa.facebook_graph_url    = 'https://graph.facebook.com'
+  raa.facebook_redirect_uri = 'http://localhost:3000/'
 end
