80113b251c2d35b1913c01f816163c196aae4e2a
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-08-03 21:24:15 +0200

    removed debug output

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -1,6 +1,4 @@
 Rails.application.config.x.facebook.tap do |facebook|
-  puts ENV['FACEBOOK_APP_ID'].inspect
-  puts ENV['FACEBOOK_APP_SECRET'].inspect
   facebook.app_id       = ENV['FACEBOOK_APP_ID']
   facebook.app_secret   = ENV['FACEBOOK_APP_SECRET']
   facebook.graph_url    = 'https://graph.facebook.com/v2.3'
